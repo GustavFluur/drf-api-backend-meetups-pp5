fb349d5776c6564ade2a99a26a308fff9a2a3e01
GustavFluur <gustavfluur@hotmail.com>
2022-12-27 15:41:22 +0000

    Add change of class name due to inconvienent long name

--- tourists/admin.py
+++ tourists/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import ExplorerProfile, Location, MeetUp
+from .models import Explorer, Location, MeetUp
 # Register your models here.
 
-admin.site.Register(ExplorerProfile)
+admin.site.Register(Explorer)
 admin.site.Register(Location)
 admin.site.Register(MeetUp)
--- tourists/models.py
+++ tourists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class ExplorerProfile(models.Model):
+class Explorer(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
